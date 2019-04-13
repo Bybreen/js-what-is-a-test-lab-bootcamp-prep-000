e27bc94b10144b6ce473d29c5e4408939db85eae
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-04-13 16:26:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 var name = "Susan"
 var height = 35
 var message = `${name} is ${height} inches tall`
+<<<<<<< HEAD
 // expect(index.height).toBeLessThan(`40`) 
  module.exports = {name, height, message}
 console.log(message)  
 
+=======
+expect(index.height).toBeLessThan(`40`)
+module.exports = {name, height, message}
+console.log(message)
+>>>>>>> 676f70750e5f5554a6340c0d036fcafeeaea62e9
